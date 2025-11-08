6b523aedda88633fa4cdd0ef7074acfc7e825e96
Victor Garcia <victor.garcia@malt.com>
2025-11-08 13:49:01 +0100

    handle multiple options

--- playground/app.vue
+++ playground/app.vue
@@ -6,6 +6,7 @@
 import { useI18n } from '#imports';
 const { t } = useI18n();
 // TODO handle nested keys
+// TODO detect plurals
 
 t('order.status', { orderId: 123, status: 1 });
 t('actions.cancel');
--- src/runtime/core/generate/templates/useI18n.ts
+++ src/runtime/core/generate/templates/useI18n.ts
@@ -3,14 +3,64 @@ import type { TranslationKey } from './type-utils';
 import type { TranslationsDictionary } from './translations';
 
 declare module 'vue-i18n' {
-  type TypedComposerTranslation<Locales = 'en-US'> = <Key extends TranslationKey>(
-    key: Key,
-    ...[options]: Key extends keyof TranslationsDictionary
-      ? TranslationsDictionary[Key] extends [never]
-        ? []
-        : [args: TranslationsDictionary[Key], options?: TranslateOptions<Locales> | string | number]
-      : []
-  ) => string;
+  interface TypedComposerTranslation<Locales = 'en-US'> {
+    <Key extends TranslationKey>(
+      key: Key,
+      ...[options]: Key extends keyof TranslationsDictionary
+        ? TranslationsDictionary[Key] extends [never]
+          ? [options: TranslateOptions<Locales>]
+          : [args: TranslationsDictionary[Key], options: TranslateOptions<Locales>]
+        : [options: TranslateOptions<Locales>]
+    ): string;
+    <Key extends TranslationKey>(
+      key: Key,
+      ...[options]: Key extends keyof TranslationsDictionary
+        ? TranslationsDictionary[Key] extends [never]
+          ? [defaultMsg: string]
+          : [args: TranslationsDictionary[Key], defaultMsg: string]
+        : [defaultMsg: string]
+    ): string;
+    <Key extends TranslationKey>(
+      key: Key,
+      ...[options]: Key extends keyof TranslationsDictionary
+        ? TranslationsDictionary[Key] extends [never]
+          ? [plural: number]
+          : [args: TranslationsDictionary[Key], plural: number]
+        : [plural: number]
+    ): string;
+    <Key extends TranslationKey>(
+      key: Key,
+      ...[options]: Key extends keyof TranslationsDictionary
+        ? TranslationsDictionary[Key] extends [never]
+          ? [list: unknown[], options: TranslateOptions<Locales>]
+          : [args: TranslationsDictionary[Key], list: unknown[], options: TranslateOptions<Locales>]
+        : [list: unknown[], options: TranslateOptions<Locales>]
+    ): string;
+    <Key extends TranslationKey>(
+      key: Key,
+      ...[options]: Key extends keyof TranslationsDictionary
+        ? TranslationsDictionary[Key] extends [never]
+          ? [list: unknown[], defaultMsg: string]
+          : [args: TranslationsDictionary[Key], list: unknown[], defaultMsg: string]
+        : [list: unknown[], defaultMsg: string]
+    ): string;
+    <Key extends TranslationKey>(
+      key: Key,
+      ...[options]: Key extends keyof TranslationsDictionary
+        ? TranslationsDictionary[Key] extends [never]
+          ? [list: unknown[], plural: number]
+          : [args: TranslationsDictionary[Key], list: unknown[], plural: number]
+        : [list: unknown[], plural: number]
+    ): string;
+    <Key extends TranslationKey>(
+      key: Key,
+      ...[options]: Key extends keyof TranslationsDictionary
+        ? TranslationsDictionary[Key] extends [never]
+          ? []
+          : [args: TranslationsDictionary[Key]]
+        : []
+    ): string;
+  }
 
   // <Key extends TranslationKey>(key: Key, defaultMsg: string, options: TranslateOptions<Locales>): string;
   // <Key extends TranslationKey>(key: Key, plural: number, options: TranslateOptions<Locales>): string;
